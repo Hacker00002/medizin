151fb6595d9d8d680d5f23a007e6eaf193944c49
muminovmuhammaqodir <muhammadqodirmuminov2@gmail.com>
2023-05-05 17:32:00 +0500

    finished cards components

--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { arr } from "./data";
+import Tooltip from "@mui/material/Tooltip";
 
 const Cards = () => {
   return (
@@ -9,19 +10,36 @@ const Cards = () => {
           <div key={elem.id}>
             <div className="img">
               <img
-                className="hoverImg"
+                className="hoverImg cursor-pointer"
                 src={elem?.img}
                 alt=""
                 width={295}
                 height={295}
               />
               <img
-                className="hoverLastImg"
+                className="hoverLastImg cursor-pointer"
                 src={elem?.img2}
                 alt=""
                 width={295}
                 height={295}
               />
+              <div className="absolute bottom-0 flex items-center gap-3 buttonHover">
+                <Tooltip title="Soldout" arrow placement="top">
+                  <button className="rounded-full w-[48px] h-[48px] leading-[48px] bg-[#333] text-[#fff] mb-[20px] text-center flex items-center justify-center hover:bg-[#4e97fd] transition-all">
+                    <i class="fa-solid fa-bag-shopping"></i>
+                  </button>
+                </Tooltip>
+                <Tooltip title="Add to wishlist" arrow placement="top">
+                  <button className="rounded-full w-[48px] h-[48px] leading-[48px] bg-[#333] text-[#fff] mb-[20px] text-center flex items-center justify-center hover:bg-[#4e97fd] transition-all">
+                    <i class="fa-regular fa-heart"></i>
+                  </button>
+                </Tooltip>
+                <Tooltip title="Compare" arrow placement="top">
+                  <button className="rounded-full w-[48px] h-[48px] leading-[48px] bg-[#333] text-[#fff] mb-[20px] text-center flex items-center justify-center hover:bg-[#4e97fd] transition-all">
+                    <i class="fa-solid fa-signal"></i>
+                  </button>
+                </Tooltip>
+              </div>
             </div>
             <div className="flex flex-col gap-[5px]">
               <a
@@ -31,7 +49,6 @@ const Cards = () => {
                 {elem?.type}
               </a>
               <h3 className="font-[400] text-[18px] hover:text-[#4e97fd] cursor-pointer">
-                {" "}
                 {elem?.name}
               </h3>
               <div className="flex items-center gap-2">
--- src/components/tagsbar/colors.jsx
+++ src/components/tagsbar/colors.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Tooltip from "@mui/material/Tooltip";
 
 const Colors = () => {
   return (
@@ -6,15 +7,33 @@ const Colors = () => {
       <h3 className="text-[20px] font-[500] mb-[20px] text-[#323232]">Color</h3>
       <div>
         <div className="flex items-center flex-wrap gap-[15px]">
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[red] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[green] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[blue] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[yellow] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[white] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[gold] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[gray] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[magenta] border-[3px]"></span>
-          <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[maroon] border-[3px]"></span>
+          <Tooltip title="Red" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[red] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Green" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[green] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Blue" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[blue] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Yellow" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[yellow] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="White" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[white] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Gold" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[gold] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Gray" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[gray] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Magenta" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[magenta] border-[3px]"></span>
+          </Tooltip>
+          <Tooltip title="Maroon" arrow placement="top">
+            <span className="w-[30px] h-[30px] rounded-full cursor-pointer !bg-[maroon] border-[3px]"></span>
+          </Tooltip>
         </div>
       </div>
     </div>
